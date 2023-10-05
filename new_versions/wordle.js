c7062c5fb38b4d187f13f75ab7d1000baccf889a
Ida Norstrom <norstrom.ida@gmail.com>
2023-10-05 12:35:57 +0200

    Made a version using divs instead of a table of inputs

--- new_versions/wordle.js
+++ new_versions/wordle.js
@@ -1,11 +1,21 @@
 const loadingDiv = document.querySelector(".info-bar")
+const letterArray = document.querySelectorAll(".letter-box")
+const ANSWER_LENGTH = 5
+const ROUNDS = 6
+const WORD_URL = "https://words.dev-apis.com/word-of-the-day"
+const RANDOM_WORD_URL = "https://words.dev-apis.com/word-of-the-day?random=1"
+const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word"
+
 
 async function init() {
-    const WORD_URL = "https://words.dev-apis.com/word-of-the-day"
-    const RANDOM_WORD_URL = "https://words.dev-apis.com/word-of-the-day?random=1"
-    const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word"
-    let isLoading = true
+    const startButton = document.querySelector(".start-btn")
+    startButton.addEventListener("click", startClick)
+
+    let guessedWord = ''
+    let currentRow = 0
     let done = false
+    let isLoading = true
+    setLoading(isLoading)
 
     // getting the word
     const response = await fetch(WORD_URL)
@@ -15,199 +25,112 @@ async function init() {
     isLoading = false
     setLoading(isLoading)
 
-    let guessedWord = ''
-    let currentRow = 0
-    let count = 0
-    let letterDict = {}
-    let value4 = ''
-
-    const startButton = document.querySelector(".start-btn")
-    startButton.addEventListener("click", startClick)
-
-    
-
-    const letters = document.querySelectorAll("letter-box")
-    console.log(letters)
-    container.onkeyup = function(e) {
-        console.log(e)
-        if (done) {
+    async function commitAnswer() {
+        if (guessedWord.length < ANSWER_LENGTH) {
+            // do nothing
             return
         }
-        if (parseInt(e.target.parentNode.parentNode.id) != currentRow || parseInt(e.target.id) === 0) {
-            guessedWord = ''
-            currentRow = e.target.parentNode.parentNode.id
-            count = 0
-            letterDict = {}
-        }
-        
-        let target = e.srcElement || e.target
-        let myLength = target.value.length
-        
-        
-        if (myLength >= 1) {
-            if (parseInt(target.id) === 4 && correctWord.indexOf(target.value) === 4) {
-                value4 = target.value
-            }
-            if (target.parentNode.nextElementSibling != null && target.parentNode.nextElementSibling.children[0].value != "") {
-                let tempDict = {}
-                if (value4 != null) {
-                    tempDict[4] = value4
-                    for (let i = 0; i < 4; i++) {
-                        tempDict[i] = target.parentNode.parentNode.children[i].children[0].value
-                    }
-                } else {
-                    for (let i = 0; i < 5; i++) {
-                        tempDict[i] = target.parentNode.parentNode.children[i].children[0].value
-                        
-                    }
-                }
-                guessedWord = ''
-                for (let i = 0; i < 5; i++) {
-                    guessedWord += tempDict[i]
-                }
-            } else if (guessedWord.length < 5) {
-                console.log(value4)
-                guessedWord += target.value.toLowerCase()
-            }
-            console.log(guessedWord)
-            if (guessedWord.length === 5) {
-                let marked = {}
-                for (let i = 0; i < guessedWord.length; i++) {
-                    if (correctWord.includes(guessedWord[i])) {
-                        if (countOccurences(correctWord, guessedWord[i]) > 1) {
-                            if (guessedWord[i] === correctWord[i]) {
-                                letterDict[i] = "ac"
-                            } else if (i === getPosition(correctWord, guessedWord[i], 2)){
-                                letterDict[i] = "ac"
-                            } else if (i === getPosition(correctWord, guessedWord[i], 3)){
-                                letterDict[i] = "ac"
-                            } else if (i != correctWord.indexOf(guessedWord[i]) ){
-                                letterDict[i] = "cl"
-                            }
-                        } else {
-                            if (i === correctWord.indexOf(guessedWord[i]) && !marked[guessedWord[i]]) {
-                                letterDict[i] = "ac"
-                                marked[guessedWord[i]] = true 
-                            } else if (i != correctWord.indexOf(guessedWord[i])) {
-                                if (correctWord.includes(guessedWord[i]) && !marked[guessedWord[i]]) {
-                                    letterDict[i] = "cl"
-                                    marked[guessedWord[i]] = true
-                                } else{
-                                    letterDict[i] = "w"
-                                }
-                            }
-    
-                        }
-                    } else {
-                        letterDict[i] = "w"
-                    }
-                    
-                }
-            }/*  else { */
-            count = Object.keys(letterDict).length
-            
-            if (!isLetter(e.key)) {
-                e.preventDefault()
-                return
-            } else {
-                let next = target.parentNode
-                let row = target.parentNode.parentNode
-                while (next = next.nextElementSibling) {
-                    if (next.tagName.toLowerCase() === "td") {
-                        next.children[0].focus()
-                        break
-                    }
-                }
-                if (count === 5) {
-                    let parentNode = target.parentNode.parentNode
-                    validateWord(VALIDATE_WORD_URL, guessedWord).then(setValidation).then(data => {
-                        valid = data.validWord
-                        if (!valid) {
-                            for (let i = 0; i < 5; i++) {
-                                parentNode.children[i].children[0].classList.add("not-valid")
-                            }
-                        } else {
-                            for (let i = 0; i < 5; i++) {
-                                if (parentNode.children[i].children[0].classList.contains("not-valid")) {
-                                    parentNode.children[i].children[0].classList.remove("not-valid")
-                                }
-                                if (letterDict[i] === "ac"){
-                                    parentNode.children[i].children[0].classList.add("all-correct")
-                                } else if (letterDict[i] === "cl") {
-                                    parentNode.children[i].children[0].classList.add("correct-letter")
-                                } else if (letterDict[i] === "w") {
-                                    parentNode.children[i].children[0].classList.add("wrong")
-                                }
-                            }
-                            parentNode.classList.add("blocked")
-                            if (guessedWord === correctWord) {
-                                document.querySelector("header").classList.add("celebration")
-                                document.querySelector(".winning").style.display = "block"
-                                done = true
-                            } 
-                            if (guessedWord != correctWord && parentNode.id === '5') {
-                                document.querySelector(".correct-word").innerHTML = `${correctWord}`
-                                document.querySelector(".lost").style.display = "block"
-                                done = true
-                            }
-                            if (!next && row.parentNode.rows[row.rowIndex + 1]) {
-                                setTimeout(function() {
-                                    row.parentNode.rows[row.rowIndex + 1].children[0].children[0].focus()
-                                    row.parentNode.rows[row.rowIndex + 1].classList.remove("blocked")
-                                }, 1000)
-                            } 
-                        }
-                    })
-                }
-            }
-        } else if (myLength === 0) {
-            guessedWord = guessedWord.slice(0, guessedWord.length - 1)
-            let previous = target.parentNode
-            if (previous.children[0].classList.contains("not-valid")) {
-                previous.children[0].classList.remove("not-valid")
-            }
-            while (previous = previous.previousElementSibling) {
-                if (previous.tagName.toLowerCase() === "td") {
-                    count -= 1
-                    previous.children[0].focus()
-                    break
-                }
-            }
 
-        }
-    }
-
-    async function validateWord(url, wordGuess) {
+        // validate the committed word
         isLoading = true
         setLoading(isLoading)
-        try {
-            const response = await fetch(url, {
-                method: "POST", 
-                body: JSON.stringify({
-                    word: wordGuess
-                })
-            })
-            const validation = await response.json()
-            return validation
-        } catch (error) {
-            console.error(error)
+
+        const res = await fetch(VALIDATE_WORD_URL, {
+            method: "POST",
+            body: JSON.stringify({word: guessedWord})
+        })
+        const wordObject = await res.json()
+        const valid = wordObject.validWord 
+
+        isLoading = false 
+        setLoading(isLoading)
+
+        if (!valid) {
+            for (let i = 0; i < guessedWord.length; i++) {
+                letterArray[currentRow * ANSWER_LENGTH + i].classList.remove("not-valid")
+
+                setTimeout(function() {
+                    letterArray[currentRow * ANSWER_LENGTH + i].classList.add("not-valid")
+                }, 100)
+            }
+        } else {
+            let wordMap = makeWordMap(guessedWord)
+
+        // checking and marking all correct letters
+        for (let i = 0 ; i < guessedWord.length; i++) {
+            if (guessedWord[i] === correctWord[i]) {
+                letterArray[currentRow * ANSWER_LENGTH + i].classList.add("all-correct")
+                wordMap[guessedWord[i]]--
+            }
+        }
+
+        // checking and marking correct and wrong letters
+        for (let i = 0 ; i < guessedWord.length; i++) {
+            if (guessedWord[i] === correctWord[i]) {
+                // do nothing
+            } else if (correctWord.includes(guessedWord[i]) && wordMap[guessedWord[i]] > 0) {
+                letterArray[currentRow * ANSWER_LENGTH + i].classList.add("correct-letter")
+                wordMap[guessedWord[i]]--
+            } else {
+                letterArray[currentRow * ANSWER_LENGTH + i].classList.add("wrong")
+            }
+
+        }
+
+        currentRow++
+
+        if (guessedWord === correctWord) {
+            document.querySelector(".winning").style.display = "block"
+            document.querySelector(".header").classList.add("celebration")
+            done = true
+        } 
+
+        if (currentRow === 6) {
+            document.querySelector(".lost").style.display = "block"
+            document.querySelector(".correct-word").innerText = correctWord
+            done = true
+        }
+
+        guessedWord = ''
         }
     }
     
-    function setValidation(validation) {
-        valid = validation
-        isLoading = false
-        setLoading(isLoading)
-        return valid
+    function addLetter(letter) {
+        if (guessedWord.length < ANSWER_LENGTH) {
+            guessedWord += letter
+            letterArray[currentRow * ANSWER_LENGTH + guessedWord.length - 1].innerText = letter
+        }
+        
     }
 
-    function getPosition(word, letter, index) {
-        return word.split(letter, index).join(letter).length
+    function backspace() {
+        guessedWord = guessedWord.slice(0, guessedWord.length - 1)
+        letterArray[currentRow * ANSWER_LENGTH + guessedWord.length].innerText = ''
     }
     
-    const countOccurences = (word, search) => {
-        return word.split(search).length - 1
-    }
+    document.addEventListener("keyup", (event) =>  {
+        if (done || isLoading) {
+            // do nothing
+            return
+        }
+        
+        let target = event.key
+        if (target === "Enter") {
+            console.log("enter")
+            commitAnswer()
+        } else if (target === "Backspace") {
+            backspace()
+        } else {
+            if (isLetter(target)) {
+                addLetter(target)
+            }
+        }
+    })
+        
+}
+
+function startClick(){
+    document.querySelector(".pop-up").style.display = "none"
 }
 
 // Function to test wether the user is typing a single letter
@@ -215,8 +138,16 @@ function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter)
 }
 
-function startClick(){
-    document.querySelector(".pop-up").style.display = "none"
+function makeWordMap(word) {
+    let map = {}
+    for (let i = 0; i < word.length; i++) {
+        if (map[word[i]]) {
+            map[word[i]]++
+        } else {
+            map[word[i]] = 1
+        }
+    }
+    return map
 }
 
 function closeWinningPopup() {
